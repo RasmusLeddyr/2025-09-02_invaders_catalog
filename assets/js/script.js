e5270deb2dea489e71ac772579b643ca0962c54b
RasmusLeddyr <rasmusleddyr@gmail.com>
2025-09-03 21:49:42 +0200

    nevermind github pages sucks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,21 @@
-window.addEventListener("DOMContentLoaded", async () => {
-  const invaders_list = document.querySelector(".invaders_list");
-  const wiki_page = document.querySelector(".wiki_page");
+const invaders_list = document.querySelector(".invaders_list");
+const wiki_page = document.querySelector(".wiki_page");
 
-  //
-  async function FetchData() {
-    try {
-      const response = await fetch("assets/data/entries.json");
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Could not fetch module data: ", error);
-    }
+//
+async function FetchData() {
+  try {
+    const response = await fetch("assets/data/entries.json");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Could not fetch module data: ", error);
   }
-  const data = await FetchData();
-  //
+}
+const data = await FetchData();
+//
 
-  const SmallTemplate = (key) =>
-    `
+const SmallTemplate = (key) =>
+  `
   <a class="entry_link" href="wiki.html?name=${encodeURIComponent(key.name)}">
     <div class="entry small">
      <div class="imag_cont">
@@ -31,8 +30,8 @@ window.addEventListener("DOMContentLoaded", async () => {
   </a>
   `;
 
-  const BigTemplate = (key) =>
-    `
+const BigTemplate = (key) =>
+  `
   <div class="entry full">
     ${key.images
       .map(
@@ -54,28 +53,25 @@ window.addEventListener("DOMContentLoaded", async () => {
   </div>
   `;
 
-  // create list on index, single on wiki
-  if (invaders_list) {
-    data.forEach((key) => {
-      invaders_list.insertAdjacentHTML("beforeend", SmallTemplate(key));
-    });
-  }
-  if (wiki_page) {
-    const urlParams = new URLSearchParams(window.location.search);
-    const name = urlParams.get("name");
+// create list on index, single on wiki
+if (invaders_list) {
+  data.forEach((key) => {
+    invaders_list.insertAdjacentHTML("beforeend", SmallTemplate(key));
+  });
+}
+if (wiki_page) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const name = urlParams.get("name");
 
-    if (name) {
-      const entry = data.find((e) => e.name === name);
-      if (entry) {
-        wiki_page.innerHTML = BigTemplate(entry);
-      } else {
-        wiki_page.innerHTML = `<p style="color:red">No entry found for "${name}"</p>`;
-      }
+  if (name) {
+    const entry = data.find((e) => e.name === name);
+    if (entry) {
+      wiki_page.innerHTML = BigTemplate(entry);
     } else {
-      wiki_page.innerHTML = `<p style="color:gray">No entry selected.</p>`;
+      wiki_page.innerHTML = `<p style="color:red">No entry found for "${name}"</p>`;
     }
+  } else {
+    wiki_page.innerHTML = `<p style="color:gray">No entry selected.</p>`;
   }
-  //
-
-  console.log("Data loaded:", data);
-});
+}
+//
